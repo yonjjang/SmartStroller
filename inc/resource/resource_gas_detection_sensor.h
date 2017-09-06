899a5a7efe62091249bfe7adab50d8fdbdb3825b
junkyu han <junkyu.han@samsung.com>
2017-09-06 21:08:58 +0900

    Add API for Gas Detection Sensor
    Change-Id: I58ffdf18865b721c4a7e22450c49eb2a2c071cfc

--- inc/resource/resource_gas_detection_sensor.h
+++ inc/resource/resource_gas_detection_sensor.h
@@ -19,20 +19,16 @@
  * limitations under the License.
  */
 
-#ifndef __POSITION_FINDER_RESOURCE_H__
-#define __POSITION_FINDER_RESOURCE_H__
+#ifndef __POSITION_FINDER_RESOURCE_GAS_DETECTION_SENSOR_H__
+#define __POSITION_FINDER_RESOURCE_GAS_DETECTION_SENSOR_H__
 
-#include <peripheral_io.h>
+/**
+ * @brief Reads the value of gpio connected to the gas detection sensor(fc-22).
+ * @param[in] pin_num The number of the gpio pin connected to the digital pin of gas detection sensor
+ * @param[out] out_value The value of the gpio (zero or non-zero)
+ * @return 0 on success, otherwise a negative error value
+ * @see If the gpio pin is not open, creates gpio handle before reading the value of gpio.
+ */
+extern int resource_read_gas_detection_sensor(int pin_num, int *out_value);
 
-#include "resource_internal.h"
-#include "resource/resource_illuminance_sensor.h"
-#include "resource/resource_infrared_motion_sensor.h"
-#include "resource/resource_infrared_obstacle_avoidance_sensor.h"
-#include "resource/resource_touch_sensor.h"
-#include "resource/resource_ultrasonic_sensor.h"
-#include "resource/resource_led.h"
-#include "resource/resource_vibration_sensor.h"
-#include "resource/resource_flame_sensor.h"
-#include "resource/resource_gas_detection_sensor.h"
-
-#endif /* __POSITION_FINDER_RESOURCE_H__ */
+#endif /* __POSITION_FINDER_RESOURCE_GAS_DETECTION_SENSOR_H__ */
